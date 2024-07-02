2ffca8304ffef67c2c13496b50bf770c5c7a931a
VATSAL Panchani <reactdeveloper3010@gmail.com>
2024-07-02 12:19:09 +0530

    Task && String_type

--- Loop_task.py
+++ Loop_task.py
@@ -24,5 +24,29 @@ print("first 10 odd number sum :-",sum)
 print("\n")
 
 # 4. Write a program to check if a number us divisible by 8 and 10 up to 100 numbers
+print("divisible by 8 and 10 up to 100 numbers")
+for i in range (1,101):
+    if i % 8 == 0 and i % 12 == 0:
+        print(i)
 
 # 5. Write a program to create a billing system at supermarket.
+while True:
+    name = input("Enter Customer Name: ")
+    total = 0
+    while True:
+     print("****** Enter the amount and quantity *****")
+     amount = float(input("Enter the amount: "))
+     quantity = float(input("Enter the quantity: "))
+     total += amount * quantity
+     repeat = input("do you want to add more items? (Yes/No)")
+     if repeat == "no" or repeat == "No":
+         break
+
+    print("-"*40)
+    print("Name", name)
+    print("Total Amount Pay: ", total)
+    print("-"*40)
+    print("************* Thank for Shopping ******************")
+    customer = input("Next Customers (Yes/No)")
+    if customer == "no" or customer == "NO":
+        break
---
+++ String.py
@@ -0,0 +1,25 @@
+a = "Recently, topic has/have sparked a heated debate among individuals. There is a school of thought that many mankind believe that it is a important topic"
+
+# 1. How to find string length
+print("String length",len(a))
+print("------------------------")
+
+# 2. How many times alphabet (e) is occurring
+print("alphabet (e) is occurring",a.count("o"))
+print("------------------------")
+
+# 3. Convert to uppercase and Lowercase
+print("** lower Case **")
+print(a.lower())
+print("** Upper Case **")
+print(a.upper())
+print("------------------------")
+
+# 4. Covert the following string into a title
+print("Convert to string into a title")
+print(a.title())
+print("------------------------")
+
+# 5. find the index number of "fit in".
+print("Find index number")
+print(a.find("individuals"))
